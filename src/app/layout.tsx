cf975aa0d24d97d23cca39087c588c2e732510a1
oyegbamioyefunke9-collab <oyegbamioyefunke9@gmail.com>
2026-05-27 21:23:48 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,11 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import { AppProvider } from "@/context/AppContext";
 
-export const metadata: Metadata = {
-  title: "Her-Ajo",
-  description: "Financial empowerment for women",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AppProvider>{children}</AppProvider>
+      </body>
     </html>
   );
 }
