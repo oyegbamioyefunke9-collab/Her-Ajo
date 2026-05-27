bf6febdef95ab5e5581e44e012459647ab5cd5b8
oyegbamioyefunke9-collab <oyegbamioyefunke9@gmail.com>
2026-05-28 00:19:22 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,24 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0", transform: "scale(0.95)" },
+          "100%": { opacity: "1", transform: "scale(1)" },
+        },
+        "slide-right": {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        }
+      },
+      animation: {
+        "fade-in": "fade-in 0.2s ease-out forwards",
+        "slide-right": "slide-right 0.3s ease-out forwards",
+      }
+    },
   },
   plugins: [],
 }
+
 export default config
